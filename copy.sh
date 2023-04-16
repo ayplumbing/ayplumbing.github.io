a8a6d2a99b96711710b694d155af1223413e747c
Pavel Petrenko <petrenkopavel@gmail.com>
2023-04-16 15:46:25 -0400

    Changing titles in raw fetched files to avoid file hash collisions

--- copy.sh
+++ copy.sh
@@ -1,9 +1,18 @@
 cp index.html basement-plumbing-services/index.html
+sed -i '' 's/<title>[^<]*/<title>Basement \& Outdoor Plumbing Services - A \& Y Plumbing/' basement-plumbing-services/index.html
 cp index.html basement-waterproofing/index.html
+sed -i '' 's/<title>[^<]*/<title>Basement Waterproofing - A \& Y Plumbing/' basement-waterproofing/index.html
 cp index.html bathroom-plumbing-services/index.html
+sed -i '' 's/<title>[^<]*/<title>Bathroom Plumbing Services - A \& Y Plumbing/' bathroom-plumbing-services/index.html
 cp index.html construction-and-renovations/index.html
+sed -i '' 's/<title>[^<]*/<title>Construction \& Renovations - A \& Y Plumbing/' construction-and-renovations/index.html
 cp index.html contact-form/index.html
+sed -i '' 's/<title>[^<]*/<title>Contact Form - A \& Y Plumbing/' contact-form/index.html
 cp index.html drain-services/index.html
+sed -i '' 's/<title>[^<]*/<title>Drain Services - A \& Y Plumbing/' drain-services/index.html
 cp index.html kitchen-plumbing-services/index.html
+sed -i '' 's/<title>[^<]*/<title>Kitchen Plumbing Services - A \& Y Plumbing/' kitchen-plumbing-services/index.html
 cp index.html privacy-policy/index.html
+sed -i '' 's/<title>[^<]*/<title>Privacy Policy - A \& Y Plumbing/' privacy-policy/index.html
 cp index.html services/index.html
+sed -i '' 's/<title>[^<]*/<title>Plumbing Services - A \& Y Plumbing/' services/index.html
